04c318b4ca637c58cc2b787daebccdbf4bd74760
Cleber Campos Batista <c1e6er@gmail.com>
2025-12-04 16:54:33 -0300

    fix: remove console.error and make app more resilient to missing root element

--- index.tsx
+++ index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error("Could not find root element");
-const root = ReactDOM.createRoot(rootElement);
-root.render(<React.StrictMode><App /></React.StrictMode>);
+if (!rootElement) {
+  document.body.innerHTML = '<div style="color: red; padding: 20px; font-family: system-ui;">Erro: elemento root não encontrado</div>';
+} else {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(<React.StrictMode><App /></React.StrictMode>);
+}
