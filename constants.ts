f4388ab7933eecf69bdbc170bdad85a88b2db93f
Cleber Campos Batista <c1e6er@gmail.com>
2025-12-09 17:05:04 -0300

    Use local logo asset

--- constants.ts
+++ constants.ts
@@ -1,8 +1,7 @@
 export const SUPABASE_URL = 'https://kbcziosidwyvlornbaxo.supabase.co';
 export const SUPABASE_ANON_KEY =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtiY3ppb3NpZHd5dmxvcm5iYXhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxODc5MDYsImV4cCI6MjA3OTc2MzkwNn0.qVThfoGFf2q5U0pCyLNqsNIpN362zAqF80RFuu2QgBs';
-export const LOGO_URL =
-  'https://kbcziosidwyvlornbaxo.supabase.co/storage/v1/object/sign/Migrei/Logo%20MIGREI%20COM.png?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV9hZWM5MDJjNy1mNDI1LTQ4NTItODY5ZC1kZTJkY2I2ZDQzMDQiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJNaWdyZWkvTG9nbyBNSUdSRUkgQ09NLnBuZyIsImlhdCI6MTc2NDg2Nzc0NSwiZXhwIjo0ODg2OTMxNzQ1fQ.eM9XAxNS-nC2iupM4Zsrdf1nzgPdoxsGyJZbnc6yhe4';
+export const LOGO_URL = '/Logo_MIGREI_COM.png';
 export const COMPANY_INFO = {
   name: 'MIGREI',
   slogan: 'Franqueadora Pioneira Oficial',
